e287ececc5a00d2c1d57f1823b0f0a35470948fc
felix <felix@mast.com>
2025-03-27 18:18:38 +0100

    try make dll application

---
+++ NodeGraph.NET6/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Livet;
+using Livet.Commands;
+using NodeGraph.NET6.Operation;
+using NodeGraph.NET6.ViewModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using NodeGraph.NET6.Controls;
+using System.Windows.Controls;
+
+namespace NodeGraph.NET6.ViewModels
+{
+
+  public enum GroupIntersectType
+  {
+    CursorPointVMDefine,
+    BoundingBoxVMDefine,
+  }
+
+  public enum RangeSelectionMode
+  {
+    ContainVMDefine,
+    IntersectVMDefine,
+  }
+
+  public class MainWindowViewModel : ViewModel
+  {
+  }
+}
